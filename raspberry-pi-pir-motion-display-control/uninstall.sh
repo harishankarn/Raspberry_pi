9e0d918159fc54ace1ae88f0e5226f0e89370f0f
Hari Shankar N <harishankarn04@gmail.com>
2024-09-30 23:00:12 +0530

    updates

--- raspberry-pi-pir-motion-display-control/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo systemctl disable motion-display-control.service
-sudo systemctl stop motion-display-control.service
-sudo rm -rf /etc/systemd/system/motion-display-control.service
-sudo rm -rf /usr/local/bin/motion-display-control.py
-
-echo "done."
